c145f15c19278a956566024e2bcb5567683e7ae6
g00g1y5p4 <g00g1y5p4@gmail.com>
2022-07-13 06:34:23 +0530

    feat: Need to add table for seat arrangement

--- components/BookNow.js
+++ components/BookNow.js
@@ -3,6 +3,15 @@ import style from './booknow.module.css'
 import Chair from './Chair';
 import Screen from './Screen'
 
+
+function addChair(){
+    rows = ['A','B','C','D','E','F','G','H','_','_','I','J']
+    colums = ['1','2','3','4','5','6','7','8','9','_','_','10','11']
+
+
+}
+
+
 function BookNow(props){
     return (
         <div className={style.main}>
@@ -28,8 +37,11 @@ function BookNow(props){
                 </div>
                 <p> Each seat tooks 0.0023 SOL</p>
                 <div className={style.seatBox}>
-                    
+                    {
+                        (() => {
 
+                        })()
+                    }
                 </div>
                 <Screen />
             </div>
